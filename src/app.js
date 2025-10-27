5ac6a8f8265e743cec9b817c9477f58d17909099
Jethanand <134337163+Jethanandj52@users.noreply.github.com>
2025-10-27 21:12:09 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 5000;
 // ✅ CORS Setup
 app.use(
   cors({
-    origin: "http://localhost:5173", // React frontend
+   origin: ["http://localhost:5173", "https://apiverse-frotend-3rfw.vercel.app"],
     credentials: true,
   })
 );
--- src/routes/groupInvite.js
+++ src/routes/groupInvite.js
@@ -11,8 +11,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER || 'jethanandj52@gmail.com',
-    pass: process.env.EMAIL_PASS || 'zsnvgtugeahrpnpi'
+    user: process.env.EMAIL_USER  ,
+    pass: process.env.EMAIL_PASS  
   }
 });
 
