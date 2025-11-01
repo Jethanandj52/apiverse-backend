82e280c62b4ec22dc411f810c1b9d45d776229a5
Jethanand <134337163+Jethanandj52@users.noreply.github.com>
2025-11-02 03:38:22 +0500

    first commit

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,22 +1,21 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
+ 
 
 const userAuth = async (req, res, next) => {
   try {
-    // Header se token
-    const authHeader = req.headers.authorization; // "Bearer <token>"
-    if (!authHeader) throw new Error("Token not found in headers");
+    const { token } = req.cookies;
+    if (!token) throw new Error("Token not found in cookies");
 
-    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded._id);
+    const user = await User.findById(decoded._id); // ✅ FIXED from `id` to `_id`
     if (!user) throw new Error("User not found");
 
     req.user = user;
     next();
   } catch (err) {
-    res.status(401).json({ error: err.message });
+    res.status(400).send("ERROR: " + err.message);
   }
 };
 
